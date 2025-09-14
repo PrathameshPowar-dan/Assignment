e049a2f33e8fa0b433d581bbdecadad296047e3d
PrathameshPowar-dan <powarprathamesh579@gmail.com>
2025-09-15 00:37:21 +0530

    Auth Route

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,14 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
+import AuthRouter from "./routes/auth.routes.js"
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+app.use("/api/user",AuthRouter);
 
 export {app}
---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,45 @@
+import User from "../models/user.model.js";
+import { ApiError } from "../utilities/ApiError.js";
+import { ApiResponse } from "../utilities/ApiResponse.js";
+import { AsyncHandler } from "../utilities/AsyncHandler.js";
+
+const Options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+}
+
+export const login = AsyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        throw new ApiError(400, "Email and password are required");
+    }
+
+    const user = await User.findOne({ email }).populate("tenantId");
+    if (!user) {
+        throw new ApiError(401, "Invalid credentials");
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid credentials");
+    }
+
+    const token = user.generateToken();
+    const LoggedInUser = {
+        id: user._id,
+        email: user.email,
+        role: user.role,
+        tenant: {
+            id: user.tenantId._id,
+            slug: user.tenantId.slug,
+            name: user.tenantId.name,
+            plan: user.tenantId.plan,
+        },
+    }
+
+
+    return res.status(200).cookie("Token", token, Options).json(new ApiResponse(200, LoggedInUser, "LOGGED IN SUCCESSFULLY"));
+});
