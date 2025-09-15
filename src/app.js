a146e47a253db92e81adc438b7b5aa17816c51e1
PrathameshPowar-dan <powarprathamesh579@gmail.com>
2025-09-15 22:53:04 +0530

    Frontend Link

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors({
     origin: [
         "http://localhost:5173",
         "https://*.vercel.app",
-        "https://*-your-username.vercel.app"
+        "https://assignment-frontend-iota-eight.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
